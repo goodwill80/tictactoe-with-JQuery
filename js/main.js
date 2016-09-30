21c8d15ff0350031993d8e51e2f18b83f5056731
kit jonathan ho <j.hokit80@gmail.com>
2016-09-30 14:06:00 +0800

    added logic

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ $(document).ready(function () {
 
 
 
+  var player = 1;
+
+  $(".square").on("click", function(){
+
+    var squareSelected = $(this);
+
+    if (squareSelected.hasClass('ex') || squareSelected.hasClass('oh')) {
+      alert('this sq has already selected');
+    } else {
+      if(player === 1) {
+        squareSelected.addClass('ex');
+        player = 2;
+      } else {
+        squareSelected.addClass('oh');
+        player = 1;
+      }
+    }
+  });
+
 
 
 
